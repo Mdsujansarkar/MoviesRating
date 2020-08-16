9e24261f9f30eb1c39ee23fa6781abff5d59c502
Mdsujansarkar <sujansarkar786@gmail.com>
2020-08-16 20:58:33 +0600

    api token include

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
@@ -13,10 +14,16 @@ class MoviesController extends Controller
      */
     public function index()
     {
-        $popularMovies  = Http::get('http://api.themoviedb.org/3/movie/popular')
-        ->json();
-        dd($popularMovies);
-        return view('index');
+        $popularMovies  = Http::withToken(config('services.tmdb.token'))
+        ->get('http://api.themoviedb.org/3/movie/popular')
+        ->json()['results'];
+        $moviegenres = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
+        dump($popularMovies);
+        return view('index', [
+            'popularMovies' => $popularMovies,
+        ]);
     }
 
     /**
